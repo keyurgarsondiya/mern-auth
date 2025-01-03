6b16beb5b6bdab34c9230e3bbe6e632e5b58c31e
Keyur R Garsondiya <keyurpatel8118@gmail.com>
2025-01-03 14:25:23 +0530

    Add frontend vite setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Header from './components/Header.jsx';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>MERN AUTH</h1>
+    </>
+  );
+}
+
+export default App;
